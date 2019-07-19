716998507a766edf9cc6199050bef737c32a9ed6
ZuuZaa <zumbaghirova@gmail.com>
2019-07-19 15:40:55 +0400

    commit

---
+++ parking/parkplace/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Place
+from .serializers import placeSerializer
+
+class Get_place_List(APIView):
+    def get(self, request):
+        place = Place.objects.all()
+        serialized = placeSerializer(place, many=True)
+        return Response(serialized.data)
+
+def index(request):
+    return render (request, 'index.html',{'list':list})
+ 
